42e9b154bec09b6c461315ad6ed993a899d1af06
clementPere <clement.pere.dev@gmail.com>
2023-06-17 22:44:26 +0200

    add calendar configuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,83 @@
 
 namespace App\Controller;
 
+use App\Entity\Calendar;
+use App\Repository\CalendarRepository;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function __construct(private ManagerRegistry $doctrine)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    }
+    #[Route('/', name: 'app_home')]
+    public function index(CalendarRepository $calendar): Response
+    {
+        $events = $calendar->findAll();
+
+        $rdvs = [];
+        foreach ($events as $event) {
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'allDay' => $event->isAllDay(),
+                'title' => $event->getTitle(),
+                'description' => $event->getDescription(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'borderColor' => $event->getBorderColor(),
+                'textColor' => $event->getTextColor(),
+            ];
+        }
+        $data = json_encode($rdvs);
+        return $this->render('home/index.html.twig', compact('data'));
+    }
+
+
+    #[Route('/{id}', name: 'app_event_edit', methods: 'PUT')]
+    public function majEvent(?Calendar $calendar, Request $request): Response
+    {
+
+        $donnees = json_decode($request->getContent());
+
+        if (
+            isset($donnees->title) && !empty($donnees->title) &&
+            isset($donnees->start) && !empty($donnees->start) &&
+            isset($donnees->description) && !empty($donnees->description) &&
+            isset($donnees->backgroundColor) && !empty($donnees->backgroundColor) &&
+            isset($donnees->borderColor) && !empty($donnees->borderColor) &&
+            isset($donnees->textColor) && !empty($donnees->textColor)
+        ) {
+            $code = 200;
+            if (!$calendar) {
+                $calendar = new Calendar;
+                $code = 201;
+            }
+            $calendar->setTitle($donnees->title);
+            $calendar->setDescription($donnees->description);
+            $calendar->setStart(new DateTime($donnees->start));
+            if ($donnees->allDay) {
+                $calendar->setEnd(new DateTime($donnees->start));
+            } else {
+                $calendar->setEnd(new DateTime($donnees->end));
+            }
+            $calendar->setAllDay($donnees->allDay);
+            $calendar->setBackgroundColor($donnees->backgroundColor);
+            $calendar->setBorderColor($donnees->borderColor);
+            $calendar->setTextColor($donnees->textColor);
+
+            $em = $this->doctrine->getManager();
+            $em->persist($calendar);
+            $em->flush();
+            return new Response('OK', $code);
+        } else {
+            return new Response('Données incomplètes', 404);
+        }
     }
 }
